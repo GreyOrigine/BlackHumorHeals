50ed67b4b59f2c8a8913e1a50751b5d623f59c67
GreyOrigine <ltstatu70@gmail.com>
2024-08-21 20:53:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-# Gradio 应用默认启动文件为app.py，应用代码相关的文件包含模型推理，应用的前端配置代码
-import os
-#os.system("mim install mmcv-full")
 import gradio as gr
+import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+from openxlab.model import download
 
-# download internlm2 to the base_path directory using git tool
-base_path = './BlackHumor'
-os.system(f'git clone https://code.openxlab.org.cn/GreyOrigine/BlackHumor.git {base_path}')
+base_path = './internlm2-chat-7b'
+os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
