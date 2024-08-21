8a86afd43be96a0145b3ecdd218bf90fee9b9c90
GreyOrigine <ltstatu70@gmail.com>
2024-08-21 21:18:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
 base_path = './internlm2-chat-1.8b'
-os.system(f'git clone https://code.openxlab.org.cn/GreyOrigine/BlackHumor.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/GreyOrigine/IDassistant.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
